cb40a538828c60c3297eabb16eaed0688b308185
Colin Walters <walters@verbum.org>
2019-08-21 00:17:09 +0000

    Import rpm-ostree code
    This way other applications can use this.
    
    See https://github.com/projectatomic/rpm-ostree/blob/016c1c5e627fc2a8cd3266ccda3a47a5f8992594/rust/src/openat_utils.rs

---
+++ README.md
@@ -0,0 +1,19 @@
+Helpers for the openat crate
+====
+
+See https://crates.io/crates/openat
+
+This crate defines a number of extension methods on `openat::Dir`
+with the common file utility functions that many real applications need.
+
+This code originated from https://github.com/projectatomic/rpm-ostree/blob/016c1c5e627fc2a8cd3266ccda3a47a5f8992594/rust/src/openat_utils.rs
+
+License
+---
+
+Licensed under either of
+
+* Apache License, Version 2.0,
+  (./LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0)
+* MIT license (./LICENSE-MIT or http://opensource.org/licenses/MIT)
+  at your option.
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,90 @@
+/*
+ * Copyright (C) 2018, 2019 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+use openat;
+use std::{fs, io};
+
+/// Helper functions for openat::Dir
+pub(crate) trait OpenatDirExt {
+    // IMO should propose this at least in a "utils" bit of the openat crate;
+    // Like 95% of the time I'm looking at errno (with files) it's for ENOENT,
+    // and Rust has an elegant way to map that with Option<>.  Every other
+    // error I usually just want to propagate back up.
+    fn open_file_optional<P: openat::AsPath>(&self, p: P) -> io::Result<Option<fs::File>>;
+
+    // On modern filesystems the directory entry contains the type; if available,
+    // return it.  Otherwise invoke stat().
+    fn get_file_type(&self, e: &openat::Entry) -> io::Result<openat::SimpleType>;
+}
+
+impl OpenatDirExt for openat::Dir {
+    fn open_file_optional<P: openat::AsPath>(&self, p: P) -> io::Result<Option<fs::File>> {
+        match self.open_file(p) {
+            Ok(f) => Ok(Some(f)),
+            Err(e) => {
+                if e.kind() == io::ErrorKind::NotFound {
+                    Ok(None)
+                } else {
+                    Err(e)
+                }
+            }
+        }
+    }
+
+    fn get_file_type(&self, e: &openat::Entry) -> io::Result<openat::SimpleType> {
+        if let Some(ftype) = e.simple_type() {
+            Ok(ftype)
+        } else {
+            Ok(self.metadata(e.file_name())?.simple_type())
+        }
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use std::{error, result};
+    use tempfile;
+
+    type Result<T> = result::Result<T, Box<error::Error>>;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn open_file_optional() -> Result<()> {
+        let td = tempfile::tempdir()?;
+        let d = openat::Dir::open(td.path())?;
+        assert!(d.open_file_optional("foo")?.is_none());
+        d.write_file("foo", 0o644)?.sync_all()?;
+        assert!(d.open_file_optional("foo")?.is_some());
+        Ok(())
+    }
+
+    #[test]
+    fn get_file_type() -> Result<()> {
+        let td = tempfile::tempdir()?;
+        let d = openat::Dir::open(td.path())?;
+        d.write_file("foo", 0o644)?.sync_all()?;
+        for x in d.list_dir(".")? {
+            let x = x?;
+            assert_eq!("foo", x.file_name());
+            let t = d.get_file_type(&x)?;
+            assert_eq!(openat::SimpleType::File, t);
+        }
+        Ok(())
     }
 }
