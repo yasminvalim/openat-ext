a8ff97b88ddf9073292ee2026fafe4a73eb3a5d4
Colin Walters <walters@verbum.org>
2019-08-21 01:27:01 +0000

    Fix docs, remove LGPL
    I wrote the code here originally, I'm relicensing it to dual
    Apache/MIT.

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,26 @@
-/*
- * Copyright (C) 2018, 2019 Red Hat, Inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
- */
+//! # Extension methods for openat::Dir
+//!
+//! ```
+//! use openat_ext::*;
+//! ```
+//!
+//! The `openat` crate is a low-level API, generally just exposing
+//! thin wrappers for the underlying system call.  This crate offers
+//! a number of common higher level convenience functions.
 
 use openat;
 use std::{fs, io};
 
 /// Helper functions for openat::Dir
 pub trait OpenatDirExt {
-    // IMO should propose this at least in a "utils" bit of the openat crate;
-    // Like 95% of the time I'm looking at errno (with files) it's for ENOENT,
-    // and Rust has an elegant way to map that with Option<>.  Every other
-    // error I usually just want to propagate back up.
+    /// Checking for nonexistent files (`ENOENT`) is by far the most common case of inspecting error
+    /// codes in Unix.  Rust has a nice `Option<>` type, so this helper makes use of it and returns `Ok(None)`
+    /// for nonexistent files.  All other errors are `Err`, and extant files are `Ok(Some<file>))`
+    /// of course.
     fn open_file_optional<P: openat::AsPath>(&self, p: P) -> io::Result<Option<fs::File>>;
 
-    // On modern filesystems the directory entry contains the type; if available,
-    // return it.  Otherwise invoke stat().
+    /// On modern filesystems the directory entry contains the type; if available,
+    /// return it.  Otherwise invoke `stat()`.
     fn get_file_type(&self, e: &openat::Entry) -> io::Result<openat::SimpleType>;
 }
 
